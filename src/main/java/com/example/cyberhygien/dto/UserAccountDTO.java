4abe4463798d2d717751644c52895a662f5db829
Your Name <you@example.com>
2024-03-23 02:04:47 +0600

    Create DTO (User account)

---
+++ src/main/java/com/example/cyberhygien/dto/UserAccountDTO.java
@@ -0,0 +1,31 @@
+package com.example.cyberhygien.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAccountDTO {
+
+    private Long userId;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    private String role;
+
+}
