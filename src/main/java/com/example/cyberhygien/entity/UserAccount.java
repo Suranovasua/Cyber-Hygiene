a96e21a18748863127923971bec6c03054b513f0
Your Name <you@example.com>
2024-03-22 22:58:58 +0600

    Develop entity (User Account)

---
+++ src/main/java/com/example/cyberhygien/entity/UserAccount.java
@@ -0,0 +1,41 @@
+package com.example.cyberhygien.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "user_accounts")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    private String role;
+
+
+}
