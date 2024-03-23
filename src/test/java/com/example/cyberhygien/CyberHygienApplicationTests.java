23239cf2d46c61380a7a4958516762af61d6a2f7
Your Name <you@example.com>
2024-03-23 23:23:42 +0600

    Setup OpenAPI and SwaggerUI

--- src/test/java/com/example/cyberhygien/CyberHygienApplicationTests.java
+++ src/test/java/com/example/cyberhygien/CyberHygienApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.cyberhygien;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@OpenAPIDefinition
 class CyberHygienApplicationTests {
 
 	@Test
